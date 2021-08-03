577488036f1bf704a2ecb061b2f0ffa778cd6c00
pk-cat <865404776@qq.com>
2021-08-03 11:42:28 +0800

    update bin

--- bin/zce-pages.js
+++ bin/zce-pages.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
-console.log(111111);
+// 添加参数配置路径
+process.argv.push("--cwd");
+process.argv.push(process.cwd());
+// 添加参数配置gulpfile的位置
+process.argv.push("--gulpfile");
+process.argv.push(require.resolve("..."));
+
+require("gulp/bin/gulp");
