762f4549019b806fcead078e6cf34e44a877d720
pk-cat <865404776@qq.com>
2021-08-04 22:57:50 +0800

    update  package

--- bin/zce-pages.js
+++ bin/zce-pages.js
@@ -5,6 +5,6 @@ process.argv.push("--cwd");
 process.argv.push(process.cwd());
 // 添加参数配置gulpfile的位置
 process.argv.push("--gulpfile");
-process.argv.push(require.resolve("..."));
+process.argv.push(require.resolve(".."));
 
 require("gulp/bin/gulp");
